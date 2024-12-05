6b165995302c4608063f1b054fd7890e72d2241a
unknown <btsyd1622003@gmail.com>
2024-12-05 10:56:40 +0700

    Merge remote-tracking branch 'origin/hiep' into hiep
    # Conflicts:
    #	Apptest/Apptaodanhmuc/lib/main.dart
    #	Apptest/Apptaodanhmuc/pubspec.lock
    #	Apptest/Apptaodanhmuc/pubspec.yaml
    #	Apptest/Apptaodanhmuc/test/widget_test.dart

--- Apptest/Apptaodanhmuc/lib/main.dart
+++ Apptest/Apptaodanhmuc/lib/main.dart
@@ -2,17 +2,3 @@ import 'package:apptaodanhmuc/widget/Man1.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AppIOT',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.grey, // Màu nền mặc định
-      ),
-      home: const Man1(),
-    );
-  }
-}
